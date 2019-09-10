a3584a01268f4b2e1afd9453320ce10bea3fb5f3
Nathan Rathe <nprathe10@gmail.com>
2019-09-10 19:48:31 +0000

    Automatically backed up by Learn

--- db/migrate/20190910194315_add_to_shows.rb
+++ db/migrate/20190910194315_add_to_shows.rb
@@ -1,5 +1,7 @@
 class AddToShow < ActiveRecord::Migration[5.2]
   def change
     add_column :shows, :genre, :string
+    add_column :shows, :day, :string
+    add_column :shows, :season, :string
   end
 end
